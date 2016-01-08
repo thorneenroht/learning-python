32885e24e4944adc130d5f845ec0ba33887f3e5b
thorneenroht <mattst9785@yahoo.com>
2016-01-07 22:27:57 -0500

    more coding bat warm ups

---
+++ codingBat_warmup.py
@@ -0,0 +1,35 @@
+
+def sum_double(a, b):
+	if a == b:
+		return (a + b)* 2
+	else:
+		return a + b
+
+
+
+def monkey_trouble(a , b):
+	if a == b:
+		return True
+	else:
+		return False
+
+def sleepIn(weekday, vacation):
+	if weekday == False and vacation == False:
+		return True
+	elif weekday == True and vacation == False:
+		return False
+	elif weekday == False and vacation == True:
+		return True
+
+print(sleepIn(False, True))
+print(sleepIn(True, False))
+print(sleepIn(False, False))
+
+
+print(monkey_trouble(True, True))
+print(monkey_trouble(False,False))
+print(monkey_trouble(True,False))
+
+print(sum_double(1,2))
+print(sum_double(3,2))
+print(sum_double(2,2))
